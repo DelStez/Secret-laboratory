2d684d2c90ad369451d0d9452baf8dd05bede0e8
RdrStels <kandakova0002@gmail.com>
2019-12-22 19:06:10 +0500

    UwU

--- Vernam/Program.cs
+++ Vernam/Program.cs
@@ -7,46 +7,6 @@ using System.Threading.Tasks;
 
 namespace Vernam
 {
-    //public class XORCipher
-    //{
-    //    //генератор повторений пароля
-    //    private string GetRepeatKey(string s, int n)
-    //    {
-    //        var r = s;
-    //        while (r.Length < n)
-    //        {
-    //            r += " " + r;
-    //        }
-
-    //        //foreach()
-    //        return r.Substring(0, n);
-    //    }
-
-    //    //метод шифрования/дешифровки
-    //    private string Cipher(string text, string secretKey)
-    //    {
-    //        var currentKey = GetRepeatKey(secretKey, text.Length);
-    //        var res = string.Empty;
-    //        for (var i = 0; i < text.Length; i++)
-    //        {
-    //            byte[] q = BitConverter.GetBytes(text[i]);
-    //            byte[] h = Encoding.ASCII.GetBytes(currentKey.Split(' ')[i]);
-    //            for (var j = 0; j < q[i]; j++)
-    //                res += ((char)(q[j] ^ h[j]));
-    //        }
-
-    //        return res;
-    //    }
-
-    //    //шифрование текста
-    //    //public string Encrypt(string plainText, string password)
-    //    //    => Cipher(plainText, password);
-
-    //    //расшифровка текста
-    //    public string Decrypt(string encryptedText, string password)
-    //        => Cipher(encryptedText, password);
-    //}
-
     class Program
     {
         static byte[] ConvertToByte(BitArray ba3)
@@ -68,19 +28,11 @@ namespace Vernam
             byte[] decimicalCode = new byte[chyphergramm.Length];
             for (int i = 0; i < (chyphergramm.Length); i++)
             {
-                decimicalCode[i] = Convert.ToByte(Convert.ToInt32(g[i]));
+                decimicalCode[i] = Convert.ToByte(g[i]);
             }
-            g.SelectMany(BitConverter.GetBytes).ToArray();
             Encoding encoding = Encoding.GetEncoding("Windows-1251");
-            gamma = gamma.Replace(" ", "");
-                for (int j = 0; j < (chyphergramm.Length); j++)
-                {
-                    gamma +=  gamma[j];
-                    if (gamma.Length == chyphergramm.Length) break;
-                    
-                }
+        
             int z = gamma.Length;
-            byte[] gammByte = encoding.GetBytes(gamma);
             byte[] chypherByte = encoding.GetBytes(chyphergramm);
            
             BitArray ba1 = new BitArray(decimicalCode);
@@ -93,18 +45,6 @@ namespace Vernam
         static void Main(string[] args)
         {
             VernamChypher("18 1 2 5 7", "ЯДБЛГЙРРЕЧНУВЖЙВД");
-
-            //var x = new XORCipher();
-            //Console.Write("Введите текст сообщения: ");
-            //var message = "ЯДБЛГЙРРЕЧНУВЖЙВД";
-            //Console.Write("Введите пароль: ");
-            //var pass = "18 1 2 5 7";
-            ////foreach(var n in pass.Split)
-            ////byte[] q = Encoding.ASCII.GetBytes(message);
-            ////var encryptedMessageByPass = x.Encrypt(message, pass);
-            ////Console.WriteLine("Зашифрованное сообщение {0}", encryptedMessageByPass);
-            //Console.WriteLine("Расшифрованное сообщение {0}", x.Decrypt(message, pass));
-            //Console.ReadLine();
 
         }
     }
