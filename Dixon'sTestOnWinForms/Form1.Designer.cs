209133526b2ae078786d5bb08fd6e0f59561da58
RdrStels <kandakova0002@gmail.com>
2019-12-30 21:22:35 +0500

    Dixon's test

---
+++ Dixon'sTestOnWinForms/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Dixon_sTestOnWinForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.ForeColor = System.Drawing.Color.Lime;
+            this.groupBox1.Location = new System.Drawing.Point(-4, 116);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(472, 286);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Данные:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(268, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Кол-во потоков";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(267, 29);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(195, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 84);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(456, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Очистить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(456, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Факторизация";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(6, 29);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(195, 20);
+            this.textBox2.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Число";
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.SystemColors.MenuText;
+            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listBox1.ForeColor = System.Drawing.Color.Lime;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(4, 22);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(468, 260);
+            this.listBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(467, 401);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Dixon'sTestOnWinForms/Form1.cs
@@ -0,0 +1,390 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+using System.Numerics;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+
+
+namespace Dixon_sTestOnWinForms
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static long[] ExtendedGCD(long a, long b)
+        {
+            long[] result = new long[3];
+            if (b == 0)
+            {
+                result[0] = a;
+                result[1] = 1;
+                result[2] = 0;
+                return result;
+            }
+            var sub_res = ExtendedGCD(b, a % b);
+            result[0] = sub_res[0];
+            result[1] = sub_res[2];
+            result[2] = sub_res[1] - (a / b) * sub_res[2];
+            return result;
+        }
+        public class SmoothInfo
+        {
+            public long a;
+            public long b;
+            public List<long> aVec = new List<long>();
+            public List<long> epsVec = new List<long>();
+        }
+        public static bool IsPower(int n)
+        {
+            if ((-n & n) == n)
+            {
+                return true;
+            }
+            var lgn = 1 + (deciToBinary(Math.Abs(n)).ToString().Length - 2);
+            for (int b = 2; b < lgn; b++)
+            {
+                var lowa = 1L;
+                var higha = 1L << (lgn / b + 1);
+                while (lowa < (higha - 1))
+                {
+                    var mida = (lowa + higha) >> 1;
+                    var ab = FastExponentiation(mida, b);
+                    if (ab > n)
+                    {
+                        higha = mida;
+                    }
+                    else if (ab < n)
+                    {
+                        lowa = mida;
+                    }
+                    else
+                    {
+                        return true; //mida ^ b
+                    }
+                }
+            }
+            return false;
+        }
+        private static long FastExponentiation(long a, int n)
+        {
+            long ans = 1;
+            while (n > 0)
+            {
+                if ((n & 1) > 0)
+                {
+                    ans = ans * a;
+                }
+                a = a * a;
+                n = n >> 1;
+            }
+            return ans;
+        }
+
+        private static int deciToBinary(int num)
+        {
+            int bin;
+            if (num != 0)
+            {
+                bin = (num % 2) + 10 * deciToBinary(num / 2);
+                return bin;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+       
+        public static IEnumerable<int> SieveOfEratosthenes(int num)
+        {
+            return Enumerable.Range(1, Convert.ToInt32(Math.Floor(Math.Sqrt(num))))
+                .Aggregate(Enumerable.Range(1, num).ToList(),
+                (result, index) =>
+                {
+                    result.RemoveAll(i => i > result[index] && i % result[index] == 0);
+                    return result;
+                }
+                );
+        }
+        private void Log(string text)
+        {
+            
+            listBox1.BeginInvoke(new MethodInvoker(() => listBox1.Items.Add(text)));
+        }
+        public static double LofN(double N)
+        {
+            return Math.Exp(Math.Sqrt(Math.Log(N, Math.E) * Math.Log(Math.Log(N, Math.E), Math.E)));
+        }
+        public static bool IsSmooth(long num, List<long> baseFactor, out List<long> factors)
+        {
+            try
+            {
+                var simpleFactors = new List<long>();
+                int b, c;
+
+                while ((num % 2) == 0)
+                {
+                    num = num / 2;
+                    simpleFactors.Add(2);
+                }
+                b = 3; c = (int)Math.Sqrt(num) + 1;
+                while (b < c)
+                {
+                    if ((num % b) == 0)
+                    {
+                        if (num / b * b - num == 0)
+                        {
+                            simpleFactors.Add(b);
+                            num = num / b;
+                            c = (int)Math.Sqrt(num) + 1;
+                        }
+                        else
+                        {
+                            b += 2;
+                        }
+                    }
+                    else
+                    {
+                        b += 2;
+                    }
+                }
+
+                simpleFactors.Add(num);
+                factors = new List<long>();
+                foreach (var bf in baseFactor)
+                {
+                    factors.Add(simpleFactors.Count(x => x == bf));
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                factors = new List<long>();
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+                System.Windows.Forms.MessageBox.Show(ex.StackTrace);
+                return false;
+            }
+        }
+        public static int TheTruePowerfullGauss(List<List<double>> a, List<double> ans)
+        {
+            int n = (int)a.Count();
+            int m = (int)a[0].Count() - 1;
+            double eps = 0.0001;
+            List<int> where = new List<int>(Enumerable.Repeat(-1, m));
+            ans.AddRange(Enumerable.Repeat(-1.0, m));
+
+            for (int col = 0, row = 0; col < m && row < n; ++col)
+            {
+                int sel = row;
+                for (int i = row; i < n; ++i)
+                    if (Math.Abs(a[i][col]) > Math.Abs(a[sel][col]))
+                        sel = i;
+                if (Math.Abs(a[sel][col]) < eps)
+                    continue;
+                for (int i = col; i <= m; ++i)
+                {
+                    var value = a[sel][i];
+                    a[sel][i] = a[row][i];
+                    a[row][i] = value;
+                }
+                where[col] = row;
+
+                for (int i = 0; i < n; ++i)
+                    if (i != row)
+                    {
+                        double c = a[i][col] / a[row][col];
+                        for (int j = col; j <= m; ++j)
+                            a[i][j] -= a[row][j] * c;
+                    }
+                ++row;
+            }
+
+            for (int i = 0; i < m; ++i)
+                if (where[i] != -1)
+                    ans[i] = a[where[i]][m] / a[where[i]][i];
+            for (int i = 0; i < n; ++i)
+            {
+                double sum = 0;
+                for (int j = 0; j < m; ++j)
+                    sum += ans[j] * a[i][j];
+                if (Math.Abs(sum - a[i][m]) > eps)
+                    return 0;
+            }
+
+            for (int i = 0; i < m; ++i)
+                if (where[i] == -1)
+                    return int.MaxValue;
+            return 1;
+        }
+        private void DixonTest(int N, int numberOfInstances)
+        {
+            
+            var tasks = new List<Task>();
+            for (int ii = 0; ii < numberOfInstances; ii++)
+            {
+               
+                tasks.Add(new Task(()=>
+                {
+                    
+                    try
+                    {
+                       
+                        var primesUponN = SieveOfEratosthenes(N).ToList();
+                        if (primesUponN.Contains(N))
+                        {
+                            Log("Число " + N + " - простое!");
+                            return;
+                        }
+
+                        primesUponN.RemoveAt(0);
+
+                        var minN = (int)Math.Sqrt(N);
+                        var M = Math.Pow(LofN(N), 0.5);
+                        var primes = primesUponN.Where(x => x <= M).ToList();
+
+                        if (primes.Count == 0)
+                        {
+                            Log("Слишком маленькое число !!");
+                            return;
+                        }
+
+                        var B = new List<long>(primes.ConvertAll(i => (long)i));
+                        int h = B.Count + 1;
+
+                        var smoothed = new List<SmoothInfo>();
+                        var answerNotFound = true;
+
+                        while (answerNotFound)
+                        {
+                            int curFound = 0;
+                            smoothed.Clear();
+                            while (curFound < h)
+                            {
+                                var b = random.Next(minN + 1, N - 1);
+                                var a = long.Parse(BigInteger.ModPow(new BigInteger(b), new BigInteger(2), new BigInteger(N)).ToString());
+                                if (a == 0)
+                                {
+                                    continue;
+                                }
+
+                                var factors = new List<long>();
+                                if (IsSmooth(a, B, out factors))
+                                {
+                                    var smooth = new SmoothInfo();
+                                    smooth.a = a;
+                                    smooth.b = b;
+                                    smooth.aVec = factors;
+                                    factors.ForEach(x => smooth.epsVec.Add(x % 2));
+                                    smoothed.Add(smooth);
+                                    curFound += 1;
+                                }
+                                else
+                                {
+                                    continue;
+                                }
+                            }
+
+                            for (int first = 0; first < smoothed.Count && answerNotFound == true; first++)
+                            {
+                                for (int second = first; second < smoothed.Count && answerNotFound == true; second++)
+                                {
+                                    if (first != second)
+                                    {
+                                        var vecOne = smoothed[first];
+                                        var vecTwo = smoothed[second];
+
+                                        var deltaLeft = smoothed.Count - (smoothed.Count - first);
+                                        var deltaMiddle = second - (first + 1);
+                                        var currentAnswer = new List<double>();
+                                        var matrix = new List<List<double>>();
+                                        for (int i = 0; i < vecOne.aVec.Count; i++)
+                                        {
+                                            var vertex = new List<double>();
+                                            vertex.AddRange(Enumerable.Repeat(0.0, deltaLeft));
+                                            vertex.Add(vecOne.aVec[i]);
+                                            vertex.AddRange(Enumerable.Repeat(0.0, deltaMiddle));
+                                            vertex.Add(vecTwo.aVec[i]);
+                                            vertex.Add(0);
+                                            matrix.Add(vertex);
+                                        }
+
+                                        TheTruePowerfullGauss(matrix, currentAnswer);
+
+                                        if (currentAnswer.Any(x => x != 0))
+                                        {
+                                            var probableX = (vecOne.b * vecTwo.b) % N;
+
+                                            var probableY = 1.0;
+                                            for (int i = 0; i < B.Count; i++)
+                                            {
+                                                var value = B[i];
+                                                var step = (vecOne.aVec[i] + vecTwo.aVec[i]) / 2.0;
+                                                probableY *= Math.Pow(value, step);
+                                            }
+
+                                            probableY = probableY % N;
+                                            if ((probableX == probableY) || (probableX == -probableY))
+                                            {
+                                                continue;
+                                            }
+                                            else
+                                            {
+                                                var u = ExtendedGCD((int)(probableX + probableY), N)[0];
+                                                var v = ExtendedGCD((int)(probableX - probableY), N)[0];
+                                                if (((u * v) == N) && (u != 1) && (v != 1))
+                                                {
+                                                    Log("Разложение найдено - (" + u + ") * (" + v + ") = " + N);
+                                                    answerNotFound = false;
+                                                }
+                                            }
+                                        }
+                                        else
+                                        {
+                                            continue;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        if (e.Message.Contains("OutOfMemoryException"))
+                        {
+                            Log("Недостаточно памяти для вычисления значения");
+                        }
+                        else
+                        {
+                            Log("Возникла ошибка:" + e.Message);
+                        }
+                        return;
+                    }
+                }));
+            }
+            tasks.ForEach(x => x.Start());
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var N = Convert.ToInt32(textBox2.Text);
+            var instances = Convert.ToInt32(textBox3.Text);
+            if (IsPower(N))
+            {
+                Log("N = " + N + " - степень простого числа");
+            }
+            else DixonTest(N, instances);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+    }
+}
