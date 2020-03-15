37e3cea279613136179c87e70f01e9b7915d783f
RdrStels <kandakova0002@gmail.com>
2020-03-15 22:45:34 +0500

    t_1

--- theories_of_information_safety/Redundancy/Form1.Designer.cs
+++ theories_of_information_safety/Redundancy/Form1.Designer.cs
@@ -28,20 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.N = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.symbol = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.button2 = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.number = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.symbol = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.p = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
@@ -107,58 +109,52 @@
             // 
             // dataGridView1
             // 
-            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.ColumnHeader;
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCellsExceptHeaders;
             this.dataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(142)))), ((int)(((byte)(123)))));
             this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.dataGridView1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.N,
+            this.number,
             this.symbol,
-            this.Column2,
-            this.Column3});
+            this.p});
             this.dataGridView1.EnableHeadersVisualStyles = false;
             this.dataGridView1.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(168)))), ((int)(((byte)(177)))));
             this.dataGridView1.ImeMode = System.Windows.Forms.ImeMode.On;
             this.dataGridView1.Location = new System.Drawing.Point(6, 19);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Sunken;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.NullValue = "counterRows";
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle2;
             this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle3.Format = "N0";
+            dataGridViewCellStyle3.NullValue = "0";
+            this.dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle3;
+            this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.dataGridView1.ShowRowErrors = false;
             this.dataGridView1.Size = new System.Drawing.Size(190, 313);
             this.dataGridView1.TabIndex = 3;
-            // 
-            // N
-            // 
-            this.N.Frozen = true;
-            this.N.HeaderText = "N";
-            this.N.Name = "N";
-            this.N.ReadOnly = true;
-            this.N.Width = 39;
-            // 
-            // symbol
-            // 
-            this.symbol.Frozen = true;
-            this.symbol.HeaderText = "Символ";
-            this.symbol.Name = "symbol";
-            this.symbol.ReadOnly = true;
-            this.symbol.Width = 70;
-            // 
-            // Column2
-            // 
-            this.Column2.Frozen = true;
-            this.Column2.HeaderText = "P(i)";
-            this.Column2.Name = "Column2";
-            this.Column2.ReadOnly = true;
-            this.Column2.Width = 46;
-            // 
-            // Column3
-            // 
-            this.Column3.Frozen = true;
-            this.Column3.HeaderText = "I";
-            this.Column3.Name = "Column3";
-            this.Column3.ReadOnly = true;
-            this.Column3.Width = 34;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // button2
             // 
@@ -180,7 +176,7 @@
             this.comboBox1.Items.AddRange(new object[] {
             "Русский - АБВГДЕЁ...",
             "Ангийский - ABCDE..."});
-            this.comboBox1.Location = new System.Drawing.Point(202, 19);
+            this.comboBox1.Location = new System.Drawing.Point(203, 19);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(130, 21);
             this.comboBox1.TabIndex = 0;
@@ -188,6 +184,30 @@
             // saveFileDialog1
             // 
             this.saveFileDialog1.CheckFileExists = true;
+            // 
+            // number
+            // 
+            this.number.HeaderText = "I";
+            this.number.Name = "number";
+            this.number.ReadOnly = true;
+            this.number.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.number.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // symbol
+            // 
+            this.symbol.HeaderText = "Символ";
+            this.symbol.Name = "symbol";
+            this.symbol.ReadOnly = true;
+            this.symbol.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.symbol.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // p
+            // 
+            this.p.HeaderText = "P(i)";
+            this.p.Name = "p";
+            this.p.ReadOnly = true;
+            this.p.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.p.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
             // 
             // Form1
             // 
@@ -222,10 +242,9 @@
         private System.Windows.Forms.SaveFileDialog saveFileDialog1;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn N;
+        private System.Windows.Forms.DataGridViewTextBoxColumn number;
         private System.Windows.Forms.DataGridViewTextBoxColumn symbol;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn p;
     }
 }
 
--- theories_of_information_safety/Redundancy/Form1.cs
+++ theories_of_information_safety/Redundancy/Form1.cs
@@ -10,6 +10,7 @@ using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace Redundancy
 {
     public partial class Form1 : Form
@@ -17,6 +18,8 @@ namespace Redundancy
         private string Rus = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ ";
         private string Eng = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
         public string fileContent;
+        public bool ItWasUsed = false;
+        List<double> oftens = new List<double>(); 
         public Form1()
         {
             InitializeComponent();
@@ -30,6 +33,8 @@ namespace Redundancy
 
         private void button1_Click(object sender, EventArgs e)
         {
+            
+                
             fileContent = string.Empty;
             var filePath = string.Empty;
 
@@ -42,7 +47,7 @@ namespace Redundancy
                 Encoding coder = Encoding.GetEncoding("windows-1251"); 
                 filePath = openFileDialog1.FileName;
                 var fileStream = openFileDialog1.OpenFile();
-                using(StreamReader reader = new StreamReader(fileStream, coder))
+                using(StreamReader reader = new StreamReader(fileStream))
                 {
                     fileContent = reader.ReadToEnd();
                 }
@@ -54,24 +59,76 @@ namespace Redundancy
 
         private void button2_Click(object sender, EventArgs e)
         {
+            if (ItWasUsed)
+            {
+                Cleaner();
+                ItWasUsed = false;
+            }
+            string alphabet = String.Empty;
             if (comboBox1.SelectedIndex == 0)
             {
                 fileContent = Regex.Replace(fileContent, @"[^а-я\s]+", "");
+                alphabet = Rus;
+
             }
-            else fileContent = Regex.Replace(fileContent, @"[^a-z\s]+", "");
+            else
+            {
+                fileContent = Regex.Replace(fileContent, @"[^a-z\s]+", "");
+                alphabet = Eng;
+            }
             string temp = fileContent.Replace("\r\n"," ");
-            //for(int i = 0; i < )
+            //double Hmax;
+            double Shannon = 0;
+            int counterRows = 1;
+            foreach (var ch in alphabet) 
+            {
+                double numbersOfChar = 0;
+                if (fileContent.Contains(ch.ToString().ToLower()))
+                { 
+                    numbersOfChar = (double)(fileContent.Length - fileContent.Replace(ch.ToString()
+                        .ToLower(), "").Length) / (double)fileContent.Length;
+                    Shannon += numbersOfChar * Math.Log(1 / numbersOfChar, 2);
+                }
+                oftens.Add(numbersOfChar);
+                string[] tempRow = { counterRows.ToString(), ch.ToString(), numbersOfChar.ToString() };
+                dataGridView1.Rows.Add(tempRow);
+                counterRows++;
+            }
+            double D = 1 - (Shannon / Hmax(alphabet.Length));
+            label1.Text = $"Избыточность = {D} ";
+            ItWasUsed = true;
             textBox2.Text = temp;
 
 
         }
+        public double Hmax(int alphabetSize)
+        {
+            double N = Math.Pow(2, alphabetSize);
+            double result = Math.Log(N, 2);
+            return result;
 
+        }
+        public void Calculate()
+        {
+
+        }
+        public void Cleaner()
+        {
+            dataGridView1.Rows.Clear();
+
+        }
+        
         private void button3_Click(object sender, EventArgs e)
         {
 
         }
 
         private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
         }
