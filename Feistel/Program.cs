a5561a14b52796fef0977b875b645a715f5ac8fd
RdrStels <kandakova0002@gmail.com>
2020-03-24 00:37:41 +0500

    FeistelBegin

---
+++ Feistel/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Feistel
+{
+    class Program
+    {
+        public static bool Mode; // булевая функция отвечающая за шифрование(true) и дешифрование 
+        public static int CountR;
+        public static byte[] temp = new byte[2];
+        static byte ConvertToByte(BitArray ba3)
+        {
+            var bytes = new byte[ba3.Length / 8];
+            ba3.CopyTo(bytes, 0);
+            return bytes[0];
+        }
+        public static byte[] CipherMode(byte[] message, byte[] key)
+        {
+            byte[] result = new byte[message.Length];
+            for (int i = 0; i < message.Length - 1; i++)
+            {
+                temp[0] = message[i];
+                temp[1] = message[i + 1];
+                Feistel();
+                result[i] = temp[0];
+                result[i + 1] = temp[1];
+            }
+            return result;
+        }
+        public static void Feistel()
+        {
+            int roundLevel = Mode ? CountR : 1;
+            BitArray Left = new BitArray(temp[0]);
+            BitArray Right = new BitArray(temp[1]);
+            for (int i = 0; i < CountR; )
+            {
+                if (i < CountR - 1)
+                {
+                    BitArray temp1 = Left;
+                    Left = Right.Xor(F());
+                    Right = temp1;
+                }
+                else
+                    Right = Right.Xor(F());
+            }
+            temp[0] = ConvertToByte(Left);
+            temp[1] = ConvertToByte(Right);
+
+        }
+        private static BitArray F()
+        {
+            return 
+        }
+        static void Main(string[] args)
+        {
+            string message = "КАНДАКОВА АНАСТАСИЯ НИКОЛАЕВНА";
+            Random r = new Random();
+            byte[] key = new byte[16];
+            CountR = key.Length;
+            r.NextBytes(key);
+            if (message.Length % 2 != 0) message += " ";
+            byte[] messageInByte = Encoding.UTF8.GetBytes(message);
+            Mode = true;
+            byte[] outputCipher = CipherMode(messageInByte, key);// Encrypt
+
+        }
+    }
+}
---
+++ Feistel/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Feistel")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Feistel")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("93a7ca5f-357c-4c74-a2cd-4a2bdf1f603c")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
