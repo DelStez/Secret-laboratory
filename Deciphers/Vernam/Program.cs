6f71bdc2fb810b75d8622d686ae5b7a67fac5194
RdrStels <kandakova0002@gmail.com>
2020-03-24 15:16:57 +0500

    Update

--- Deciphers/Vernam/Program.cs
+++ Deciphers/Vernam/Program.cs
@@ -11,34 +11,40 @@ namespace Vernam
     {
         static void Main(string[] args)
         {
-            VernamChypher("18 1 2 5 7", "ЯДБЛГЙРРЕЧНУВЖЙВД");
+            string message = "КАНДАКОВА АНАСТАСИЯ НИКОЛАЕВНА";
+            string key = "18 1 2 5 7";
+            string res = VernamChypher(key , message); //Шифруем
+            Console.WriteLine(res);
+            res = VernamChypher(key, res); // дешифруем для проверки
+            Console.WriteLine(res);
 
         }
-        
 
-        static void VernamChypher(string gamma, string chyphergramm)
+
+        static string VernamChypher(string gamma, string chyphergramm)
         {
             Encoding encoding = Encoding.GetEncoding("Windows-1251");
-            List<int> g = new List<int>(gamma.Split(' ').Select(Int32.Parse).ToArray());
+            List<int> g = new List<int>(gamma.Split(' ').Select(Int32.Parse).ToArray());//получаем список из гаммы
             byte[] chypherByte = encoding.GetBytes(chyphergramm);
-            for (int j = 0; j < (chyphergramm.Length); j++)
+            for (int j = 0; j < (chyphergramm.Length); j++) //длина гаммы должна совпадать с длинной полученного сообщения, по этому добавляем элементы 
             {
                 g.Add(g[j]);
                 if (g.Count == chyphergramm.Length) break;
 
             }
             byte[] decimicalCode = new byte[chyphergramm.Length];
-            for (int i = 0; i < (chyphergramm.Length); i++)
+            for (int i = 0; i < (chyphergramm.Length); i++)// каждый символ сообщения переводим в hex для оператора Xor это неоходиммо
             {
                 decimicalCode[i] = Convert.ToByte(g[i]);
             }
-            BitArray ba1 = new BitArray(decimicalCode);
-            BitArray ba2 = new BitArray(chypherByte);
-            BitArray ba3 = ba1.Xor(ba2);
+            BitArray ba1 = new BitArray(decimicalCode);// здесь получаем двоичный код для соообщения
+            BitArray ba2 = new BitArray(chypherByte);//     и для гаммы
+            BitArray ba3 = ba1.Xor(ba2);// гаммирование
             byte[] byteArr = ConvertToByte(ba3);
-            string str = encoding.GetString(byteArr, 0, byteArr.Length);
-            Console.WriteLine(str);
+            string str = encoding.GetString(byteArr, 0, byteArr.Length);// конвертируем и получаем обратно уже шифрованную строку
+            return str;
         }
+        //Функциия перевода из 2-ого в hex
         static byte[] ConvertToByte(BitArray ba3)
         {
             var bytes = new byte[ba3.Length / 8];
--- Feistel/Program.cs
+++ Feistel/Program.cs
@@ -9,7 +9,7 @@ namespace Feistel
 {
     class Program
     {
-        public static bool Mode; // булевая функция отвечающая за шифрование(true) и дешифрование 
+        public static bool Mode= false; // булевая функция отвечающая за шифрование(false) и дешифрование(true) 
         public static int CountR;
         public static byte[] temp = new byte[2];
         public static byte[] key = { 183, 222, 126, 50, 205, 133, 46,  50 };
@@ -28,10 +28,10 @@ namespace Feistel
         }
         public static void Feistel()
         {
-            int roundLevel = 0;
+            int roundLevel = Mode ? CountR : 0;
             byte Left = temp[0];
             byte Right = temp[1];
-            for (int i = 0; i < CountR; i++, roundLevel++)
+            for (int i = 0; i < CountR; i++, roundLevel += Mode ? -1: 1)
             {
                 if (i < CountR - 1)
                 {
@@ -39,27 +39,30 @@ namespace Feistel
                     Left = Convert.ToByte(Right^(F(Left, key[roundLevel])));
                     Right = temp1;
                 }      
+                else
+                    Right = Convert.ToByte(Right ^ (F(Left, key[roundLevel])));
             }
             temp[0] = Left;
             temp[1] = Right;
 
         }
-        private static int F(byte A, int B)
+        private static byte F(byte A, byte B)
         {
-            int result = ((Convert.ToInt32(A) + B) % (2*temp.Length));
 
+            byte result = Convert.ToByte(((A+B)^B) % 256);
             return result;
         }
         static void Main(string[] args)
         {
             string message = "КАНДАКОВА АНАСТАСИЯ НИКОЛАЕВНА";
             Random r = new Random();
-            CountR = key.Length-1;
-            //r.NextBytes(key);
+            CountR = key.Length - 1;
+            r.NextBytes(key);
             if (message.Length % 2 != 0) message += " ";
             byte[] messageInByte = Encoding.UTF8.GetBytes(message);
             byte[] outputCipher = CipherMode(messageInByte, key);// Encrypt
             Console.WriteLine(Encoding.UTF8.GetString(outputCipher, 0, outputCipher.Length));
+            Mode = true;
             outputCipher = CipherMode(outputCipher, key);// decrypt
             Console.WriteLine(Encoding.UTF8.GetString(outputCipher, 0, outputCipher.Length));
             Console.ReadLine();
