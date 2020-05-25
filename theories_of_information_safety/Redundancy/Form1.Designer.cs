1a68070c6ff61994cf2191b7b5d1e8ecd049ffab
RdrStels <kandakova0002@gmail.com>
2020-05-25 13:05:37 +0500

    апгрейд программы анализа текста

--- theories_of_information_safety/Redundancy/Form1.Designer.cs
+++ theories_of_information_safety/Redundancy/Form1.Designer.cs
@@ -28,9 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle16 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle17 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle18 = new System.Windows.Forms.DataGridViewCellStyle();
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
@@ -44,6 +44,9 @@
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
@@ -81,6 +84,7 @@
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
             this.textBox2.ReadOnly = true;
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
             this.textBox2.Size = new System.Drawing.Size(337, 399);
             this.textBox2.TabIndex = 3;
             // 
@@ -88,7 +92,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(77)))), ((int)(((byte)(119)))));
-            this.label1.Location = new System.Drawing.Point(6, 362);
+            this.label1.Location = new System.Drawing.Point(202, 58);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 4;
@@ -96,15 +100,18 @@
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.dataGridView1);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.button2);
             this.groupBox1.Controls.Add(this.comboBox1);
             this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.groupBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(77)))), ((int)(((byte)(119)))));
-            this.groupBox1.Location = new System.Drawing.Point(360, 34);
+            this.groupBox1.Location = new System.Drawing.Point(360, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(339, 404);
+            this.groupBox1.Size = new System.Drawing.Size(482, 426);
             this.groupBox1.TabIndex = 6;
             this.groupBox1.TabStop = false;
             // 
@@ -117,13 +124,13 @@
             this.dataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(142)))), ((int)(((byte)(123)))));
             this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.dataGridView1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
-            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle16.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle16.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle16.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle16.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle16.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle16.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle16;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.number,
@@ -136,21 +143,21 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Sunken;
-            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle2.NullValue = "counterRows";
-            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            dataGridViewCellStyle17.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle17.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle17.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle17.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle17.NullValue = "counterRows";
+            dataGridViewCellStyle17.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle17.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle17.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle17;
             this.dataGridView1.RowHeadersVisible = false;
             this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
-            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.Black;
-            dataGridViewCellStyle3.Format = "N0";
-            dataGridViewCellStyle3.NullValue = "0";
-            this.dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle3;
+            dataGridViewCellStyle18.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle18.Format = "N0";
+            dataGridViewCellStyle18.NullValue = "0";
+            this.dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle18;
             this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
             this.dataGridView1.ShowRowErrors = false;
             this.dataGridView1.Size = new System.Drawing.Size(190, 313);
@@ -184,7 +191,7 @@
             // 
             this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(179)))), ((int)(((byte)(200)))), ((int)(((byte)(231)))));
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Location = new System.Drawing.Point(202, 46);
+            this.button2.Location = new System.Drawing.Point(339, 19);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(129, 21);
             this.button2.TabIndex = 1;
@@ -209,12 +216,42 @@
             // 
             this.saveFileDialog1.CheckFileExists = true;
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(77)))), ((int)(((byte)(119)))));
+            this.label2.Location = new System.Drawing.Point(202, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(77)))), ((int)(((byte)(119)))));
+            this.label3.Location = new System.Drawing.Point(202, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(77)))), ((int)(((byte)(119)))));
+            this.label4.Location = new System.Drawing.Point(202, 133);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "label4";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(142)))), ((int)(((byte)(123)))));
-            this.ClientSize = new System.Drawing.Size(719, 450);
+            this.ClientSize = new System.Drawing.Size(857, 450);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
@@ -244,6 +281,9 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn number;
         private System.Windows.Forms.DataGridViewTextBoxColumn symbol;
         private System.Windows.Forms.DataGridViewTextBoxColumn p;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- theories_of_information_safety/Redundancy/Form1.cs
+++ theories_of_information_safety/Redundancy/Form1.cs
@@ -18,12 +18,53 @@ namespace Redundancy
         private string Rus = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ ";
         private string Eng = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
         public string fileContent;
+        public int totalWords;
+        public int totalSentence;
+        public int totalSyllables;
+        public int totalHardWords;
         public bool ItWasUsed = false;
+        public string alphabet;
         List<double> oftens = new List<double>(); 
         public Form1()
         {
             InitializeComponent();
             comboBox1.SelectedIndex = 0;
+        }
+        public string GetHmax()// Нахождение избыточности
+        {
+            string _temp = fileContent;
+            alphabet = String.Empty;
+            if (comboBox1.SelectedIndex == 0)
+            {
+                _temp = Regex.Replace(_temp, @"[^а-я\s]+", "");
+                alphabet = Rus;
+
+            }
+            else
+            {
+                _temp = Regex.Replace(_temp, @"[^a-z\s]+", "");
+                alphabet = Eng;
+            }
+            string temp = _temp.Replace("\r\n", " ");
+            double Shannon = 0;
+            int counterRows = 1;
+            foreach (var ch in alphabet)
+            {
+                double numbersOfChar = 0;
+                if (_temp.Contains(ch.ToString().ToLower()))
+                {
+                    numbersOfChar = (double)(_temp.Length - _temp.Replace(ch.ToString()
+                        .ToLower(), "").Length) / (double)_temp.Length;
+                    Shannon += numbersOfChar * Math.Log(1 / numbersOfChar, 2);
+                }
+                oftens.Add(numbersOfChar);
+                string[] tempRow = { counterRows.ToString(), ch.ToString(), numbersOfChar.ToString() };
+                dataGridView1.Rows.Add(tempRow);
+                counterRows++;
+            }
+            double D = 1 - (Shannon / Hmax(alphabet.Length));
+            label1.Text = $"Избыточность = {D} ";
+            return temp;
         }
         private void button1_Click(object sender, EventArgs e)
         {
@@ -51,6 +92,69 @@ namespace Redundancy
             }
         }
 
+        public void Flash()
+        {
+            string temp = fileContent;
+            String[] words = temp.Split(new char[] { ' ', '\r' ,'\n', '\0', '—' });
+            totalWords = 0;
+            totalSentence = 0;
+            totalHardWords = 0;
+            totalSyllables = 0;
+            foreach (var word in words)
+            {
+                if (word.Length != 0)
+                    totalWords++;
+            }
+            String[] s = fileContent.Split(new char[] { '!', '.', '?' });
+           
+            foreach (var word in s)
+            {
+                if (word.Length != 0)
+                    totalSentence++;
+            }
+            string reg = @"(\b[aа-яzAА-ЯZ]+)";
+            string gl = @"([aeyuioуеыаояиюэ]+)";
+            Match oneWord = Regex.Match(temp, reg);
+            int slogs = 0;
+            while (oneWord.Success)
+            {
+                Match number = Regex.Match(oneWord.Groups[1].Value, gl);
+                while (number.Success)
+                {
+                    slogs++;
+                    totalSyllables++;
+                    number = number.NextMatch();
+                }
+
+                if (slogs >= 4)
+                {
+                    slogs = 0;
+                    totalHardWords++;
+                }
+
+                oneWord = oneWord.NextMatch();
+            }
+            double flash = 0;
+            double flashKincade = 0;
+            double Gunning = 0;
+
+            if (comboBox1.SelectedIndex == 0)// для русского
+            {
+                
+                flash = 206.835 - (1.3 * Math.Round((double)(totalWords / totalSentence), 2)) - (60.1 * Math.Round((double)(totalSyllables / totalWords), 2));
+                flashKincade = ((8.38 * (double)(totalWords / totalSentence)) + (0.45 * (double)(totalSyllables / totalWords))) - 15.59;
+                Gunning = 0.4 * (0.78 * (totalWords / totalSentence) + 100 * (totalHardWords / totalWords));
+            }
+            else// для английского
+            {
+                flash = 206.835 - (1.015 * Math.Round((double)(totalWords / totalSentence), 2)) - (84.6 * (Math.Round((double)(totalSyllables / totalWords), 2)));
+                flashKincade = ((0.39 * Math.Round((double)(totalWords / totalSentence), 2)) + (11.8 * Math.Round((double)(totalSyllables / totalWords), 2))) - 15.59;
+                Gunning = 0.4 * ((totalWords / totalSentence) + (totalHardWords / totalWords));
+            }
+            label2.Text = "Флеш:" + flash.ToString();
+            label3.Text = "Флеш-Кинсайд: " + flashKincade.ToString();
+            label4.Text = "Индекс туманности Ганнинга: " + Gunning.ToString();
+        }
         private void button2_Click(object sender, EventArgs e)
         {
             if (ItWasUsed)
@@ -58,40 +162,9 @@ namespace Redundancy
                 Cleaner();
                 ItWasUsed = false;
             }
-            string alphabet = String.Empty;
-            if (comboBox1.SelectedIndex == 0)
-            {
-                fileContent = Regex.Replace(fileContent, @"[^а-я\s]+", "");
-                alphabet = Rus;
-
-            }
-            else
-            {
-                fileContent = Regex.Replace(fileContent, @"[^a-z\s]+", "");
-                alphabet = Eng;
-            }
-            string temp = fileContent.Replace("\r\n"," ");
-            //double Hmax;
-            double Shannon = 0;
-            int counterRows = 1;
-            foreach (var ch in alphabet) 
-            {
-                double numbersOfChar = 0;
-                if (fileContent.Contains(ch.ToString().ToLower()))
-                { 
-                    numbersOfChar = (double)(fileContent.Length - fileContent.Replace(ch.ToString()
-                        .ToLower(), "").Length) / (double)fileContent.Length;
-                    Shannon += numbersOfChar * Math.Log(1 / numbersOfChar, 2);
-                }
-                oftens.Add(numbersOfChar);
-                string[] tempRow = { counterRows.ToString(), ch.ToString(), numbersOfChar.ToString() };
-                dataGridView1.Rows.Add(tempRow);
-                counterRows++;
-            }
-            double D = 1 - (Shannon / Hmax(alphabet.Length));
-            label1.Text = $"Избыточность = {D} ";
             ItWasUsed = true;
-            textBox2.Text = temp;
+            textBox2.Text = GetHmax();
+            Flash();
 
 
         }
