126640b07c5d0dcdc7e0b8ed472d04876beb1659
RdrStels <kandakova0002@gmail.com>
2020-05-30 16:48:46 +0500

    I began to create the AVL-tree

--- BinaryAndRedBlackTree/AVLandRedBlackTrees/Core/BST.cs
+++ BinaryAndRedBlackTree/AVLandRedBlackTrees/Core/BST.cs
@@ -8,6 +8,35 @@ namespace AVLandRedBlackTrees.Core
 {
     public class BST
     {
+        public class BSTNode
+        {
+            public int key;
+            public BSTNode parent;
+            public BSTNode leftChild;
+            public BSTNode rightChild;
+            private readonly int deepth;
+
+            public BSTNode(BSTNode parent, int keyValue)
+            {
+                this.parent = parent;
+                this.key = keyValue;
+
+                this.leftChild = null;
+                this.rightChild = null;
+
+                if (this.parent == null)
+                    this.deepth = 0;
+                else
+                    this.deepth = this.parent.deepth + 1;
+            }
+            public BSTNode Parrent => parent;
+            public int KeyValue => key;
+            public int Deepth => deepth;
+            public BSTNode LeftBSTNode { get => leftChild; set => leftChild = value; }
+            public BSTNode RightBSTNode { get => rightChild; set => rightChild = value; }
+
+            public bool IsRoot() { return this.Parrent == null; }
+        }
         public static BSTNode root;
         public int deepth;
 
